b65f32b0f53b516e55858ef0a6d371028615ebc2
Fervab <fer.valdes23@gmail.com>
2017-04-16 17:49:37 -0500

    game of threes/ C++

---
+++ Easy/Source1.cpp
@@ -0,0 +1,9 @@
+#inlcude <iostream>
+
+using namespace std;
+
+
+int main()
+{ 
+	cout << "hello world" << endl;
+}
---
+++ Easy/[2015-11-02] Challenge #239 [Easy] A Game of Threes.py
@@ -0,0 +1,15 @@
+#print (userNum)
+userNum = 50
+
+print (userNum)
+
+while (userNum != 1):
+	if (userNum % 3 == 0):
+		print ("in func")
+		userNum = userNum / 3
+	elif (userNum + 1 == 0):
+		userNum = userNum + 1
+	elif (userNum - 1 == 0):
+		userNum = userNum - 1
+
+print ("1")
